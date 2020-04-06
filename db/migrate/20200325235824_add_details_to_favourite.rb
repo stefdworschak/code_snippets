fc5e706789fd59668337685870e023dc1bfe8a9b
Stefan Dworschak <stef.dworschak@gmail.com>
2020-04-06 22:19:07 +0000

    remove unneeded model

--- db/migrate/20200325235824_add_details_to_favourite.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToFavourite < ActiveRecord::Migration[5.2]
-  def change
-    add_column :favourites, :description, :string
-    add_reference :favourites, :snippet, foreign_key: true
-    add_reference :favourites, :user, foreign_key: true
-  end
-end
