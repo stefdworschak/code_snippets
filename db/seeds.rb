206c5ecb7baaf8a7a04a79955feb90a60e2a149f
Stefan Dworschak <stef.dworschak@gmail.com>
2020-04-19 16:19:13 +0000

    changed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 admin_user = User.new
 admin_user.email = 'admin@csnippets.com'
-admin_user.password = 'Password1'
-admin_user.password_confirmation = 'Password1'
+admin_user.password = 'Password-1'
+admin_user.password_confirmation = 'Password-1'
 admin_user.admin = true
 admin_user.save!
 
 test_user = User.new
 test_user.email = 'user@csnippets.com'
-test_user.password = 'Password1'
-test_user.password_confirmation = 'Password1'
+test_user.password = 'Password-1'
+test_user.password_confirmation = 'Password-1'
 test_user.admin = false
 test_user.save!
