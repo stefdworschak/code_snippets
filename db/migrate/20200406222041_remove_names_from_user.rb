30375f96979d1406fbcacac0b6186777a70a841a
Stefan Dworschak <stef.dworschak@gmail.com>
2020-04-19 15:34:03 +0000

    fixing migration

--- db/migrate/20200406222041_remove_names_from_user.rb
+++ db/migrate/20200406222041_remove_names_from_user.rb
@@ -2,6 +2,6 @@ class RemoveNamesFromUser < ActiveRecord::Migration[5.2]
   def change
     remove_column :profiles, :firstname
     remove_column :profiles, :lastname
-    remove_column :profiles, :Address
+    remove_column :profiles, :address
   end
 end
